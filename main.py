f141028d235268e2202de498c5dd3a6a265b8160
Esteban Duran <duran93@outlook.com>
2017-04-10 19:49:53 -0500

    Adds more content to the notebook.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-
-import numpy
 import dlib
+import numpy as np
+from skimage import io
+
+face_detector = dlib.get_frontal_face_detector()
+
+win = dlib.image_window()
+
+with open("images/all_file.txt") as file:
+    for line in file:
+        image_name = line
+        
+        # Next we load the image into an array.
+        image = io.imread("images/" + image_name[:-1])
+        
+        # Run HOG face detector on the image. 
+        # This will result in a set of bounding boxes on the faces.
+        detected_faces = face_detector(image, 1)
+        
+        # Opens a window with a red rectangle overlaid on each face of every frame. 
+        win.clear_overlay()
+        win.set_image(image)
+        #win.add_overlay(detected_faces)
+    
